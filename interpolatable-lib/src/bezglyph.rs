ba59cee0e284c76195cc4811c9ece65e1cc7edd0
Simon Cozens <simon@simon-cozens.org>
2025-05-07 14:21:02 +0100

    Document, prepare for 1.0.0 release of library

--- interpolatable-lib/src/bezglyph.rs
+++ interpolatable-lib/src/bezglyph.rs
@@ -1,18 +1,25 @@
 use kurbo::BezPath;
 
 #[derive(Default, Debug)]
+/// A `BezGlyph` is a collection of `BezPath`s, which represent the outline of a glyph.
+/// It is used to store the paths of a glyph in a vector, allowing for multiple paths
+/// to be stored in a single glyph.
 pub struct BezGlyph(pub(crate) Vec<BezPath>);
 
 impl BezGlyph {
+    /// Creates a new `BezGlyph` from a vector of `BezPath`s.
     pub fn new_from_paths(b: Vec<BezPath>) -> Self {
         BezGlyph(b)
     }
     #[allow(clippy::should_implement_trait)]
+    /// Adds a new `BezPath` to the `BezGlyph` and returns a mutable reference to it.
     pub fn next(&mut self) -> &mut BezPath {
         self.0.push(BezPath::new());
         #[allow(clippy::unwrap_used)] // We just added it
         self.0.last_mut().unwrap()
     }
+    /// Returns a mutable reference to the current `BezPath`. If there are no paths,
+    /// it creates a new one and returns it.
     pub fn current(&mut self) -> &mut BezPath {
         if self.0.is_empty() {
             self.0.push(BezPath::new());
@@ -21,6 +28,7 @@ impl BezGlyph {
         self.0.last_mut().unwrap()
     }
 
+    /// Iterates over the paths in the `BezGlyph`, returning an iterator of references to `BezPath`.
     pub fn iter(&self) -> impl Iterator<Item = &BezPath> {
         self.0.iter()
     }
